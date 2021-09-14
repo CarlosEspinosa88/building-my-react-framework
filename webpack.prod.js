97576498856f64f768b3f59204f03cb81839ff03
Carlos Espinosa <carloespinosa88@gmail.com>
2021-09-14 13:10:01 -0500

    feat: add webpack config

---
+++ webpack.prod.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const basePath = __dirname;
+const distPath = 'dist';
+ 
+const indextInput = './src/index.html';
+const indexOutput = 'index.html';
+
+const webpackProdConfig = {
+  mode: 'production',
+  entry: './src/index.js',
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    clean: true,
+    publicPath: '/',
+    path: path.join(basePath, distPath),
+    filename: '[chunkhash][name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: indexOutput, 
+      template: indextInput,
+      hash: true,
+    })
+  ],
+  devtool: 'source-map',
+};
+
+module.exports = webpackProdConfig;
